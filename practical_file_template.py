455d801576d750762b43eac724dc2d7f866e134e
Samaksh Khatri <samakshkhatri14@gmail.com>
2020-07-26 16:15:33 +0530

    Update practical_file_template.py
    Added GUI support using TKinter

--- practical_file_template.py
+++ practical_file_template.py
@@ -1,9 +1,12 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import scrolledtext
 from docx import Document
 from docx.shared import Pt
 from docx.shared import Inches
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.enum.style import WD_STYLE_TYPE
-from docx2pdf import convert
+import tkinter
 
 #Initialsing Document object
 document = Document()
@@ -51,57 +54,141 @@ content_font.name='Times New Roman'
 content_font.size = Pt(12)
 
 
+window = Tk()
 
-#----------------------------------------
+window.title("Welcome to File Generator")
 
-#Actual Content starts
+window.geometry('650x500')
 
-#----------------------------------------
+file=""
+out_file=""
 
-#Variables for Data
+def choose_prog():
+    global file
+    file=filedialog.askopenfilename()
+    prog_path_label.configure(text=file)
 
-header_data="18DCS035\t\tCS347"     #Insert Header Here
-footer_data = "DEPSTAR(CSE)"
-title_data="Practical 1"            #Insert Heading Here
-aim_data="To create a Python program to generate word files"  #Insert Aim Here
-program_code_data="Code here"       #Insert Code Here
-picture_path="C:/Users/Admin/Desktop/photo.jpg"
-conclusion_data="Conclusion"        #Insert Conclusion Here
+def choose_out():
+    global out_file
+    out_file=filedialog.askopenfilename()
+    out_path_label.configure(text=out_file)
 
-
-#Adding header
-header_content = header.paragraphs[0]
-header_content.text = header_data
-header_content.style = document.styles["Header"]
-
-footer_content = footer.paragraphs[0]
-footer_content.text= footer_data
-footer_content.style = document.styles["Footer"]
-
-#Adding title
-title = document.add_paragraph(title_data,style='Practical_Number')
-title.alignment=WD_ALIGN_PARAGRAPH.CENTER
-
-#Adding sub-title
-aim = document.add_paragraph("Aim",style='Sub_Head')
-
-#Adding content to sub-title
-aim_content = document.add_paragraph(aim_data,style='Content')
-aim_content.alignment= WD_ALIGN_PARAGRAPH.JUSTIFY
-program_code = document.add_paragraph("Program Code",style='Sub_Head')
+def generate_file():
+    global file
+    global out_file
+    print("ID Number : ",id_entry.get())
+    print("Subject : ",subj_entry.get())
+    print("Heading : ",head_entry.get())
+    print("Aim : ",aim_entry.get())
+    print("Program Code: ",prog_entry.get("1.0",tkinter.END))
+    print("Output Screenshot Path : ",out_file)
+    print("Conclusion : ",concl_entry.get())
+    header_data=id_entry.get()+"\t\t"+subj_entry.get()     #Insert Header Here
+    footer_data = "DEPSTAR(CSE)"
+    title_data=head_entry.get()            #Insert Heading Here
+    aim_data=aim_entry.get()  #Insert Aim Here
+    program_code_data=prog_entry.get("1.0",tkinter.END)       #Insert Code Here
+    picture_path=out_file
+    conclusion_data=concl_entry.get()       #Insert Conclusion Here
 
 
-program_code_content = document.add_paragraph(program_code_data,style='Content')
-program_code_content.alignment= WD_ALIGN_PARAGRAPH.JUSTIFY
-output = document.add_paragraph('Output',style='Sub_Head') 
-document.add_picture(picture_path,width=Inches(1.25))
+    #Adding header
+    header_content = header.paragraphs[0]
+    header_content.text = header_data
+    header_content.style = document.styles["Header"]
 
-picture = document.paragraphs[-1]
-picture.alignment= WD_ALIGN_PARAGRAPH.CENTER
-conclusion = document.add_paragraph('Conclusion',style='Sub_Head')
-conclusion_content = document.add_paragraph(conclusion_data,style='Content')
-conclusion_content.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+    footer_content = footer.paragraphs[0]
+    footer_content.text= footer_data
+    footer_content.style = document.styles["Footer"]
 
-document.save('template_test.docx')
+    #Adding title
+    title = document.add_paragraph(title_data,style='Practical_Number')
+    title.alignment=WD_ALIGN_PARAGRAPH.CENTER
 
-convert('template_test.docx')
+    #Adding sub-title
+    aim = document.add_paragraph("Aim",style='Sub_Head')
+
+    #Adding content to sub-title
+    aim_content = document.add_paragraph(aim_data,style='Content')
+    aim_content.alignment= WD_ALIGN_PARAGRAPH.JUSTIFY
+    program_code = document.add_paragraph("Program Code",style='Sub_Head')
+
+
+    program_code_content = document.add_paragraph(program_code_data,style='Content')
+    program_code_content.alignment= WD_ALIGN_PARAGRAPH.JUSTIFY
+    output = document.add_paragraph('Output',style='Sub_Head') 
+    document.add_picture(picture_path,width=Inches(1.25))
+
+    picture = document.paragraphs[-1]
+    picture.alignment= WD_ALIGN_PARAGRAPH.CENTER
+    conclusion = document.add_paragraph('Conclusion',style='Sub_Head')
+    conclusion_content = document.add_paragraph(conclusion_data,style='Content')
+    conclusion_content.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+
+    document.save('template_test.docx')
+    window.destroy()
+    
+
+id_label = Label(window,text="Enter your ID Number : ",anchor=W,justify=LEFT,width=30)
+id_label.grid(column=0,row=0)
+
+id_entry = Entry(window,width=50)
+id_entry.grid(column=1,row=0)
+
+Label(window,text="").grid(column=0,row=1)
+
+subj_label = Label(window,text="Enter Subject Code and Name : ",anchor=W,justify=LEFT,width=30)
+subj_label.grid(column=0,row=2)
+
+subj_entry = Entry(window,width=50)
+subj_entry.grid(column=1,row=2)
+
+Label(window,text="").grid(column=0,row=3)
+
+head_label = Label(window,text="Enter Heading : ",anchor=W,justify=LEFT,width=30)
+head_label.grid(column=0,row=4)
+
+head_entry = Entry(window,width=50)
+head_entry.grid(column=1,row=4)
+
+Label(window,text="").grid(column=0,row=5)
+
+aim_label = Label(window,text="Enter Aim : ",anchor=W,justify=LEFT,width=30)
+aim_label.grid(column=0,row=6)
+
+aim_entry = Entry(window,width=50)
+aim_entry.grid(column=1,row=6)
+
+Label(window,text="").grid(column=0,row=7)
+
+prog_label = Label(window,text="Program Code : ",anchor=W,justify=LEFT,width=30)
+prog_label.grid(column=0,row=8)
+
+prog_entry = scrolledtext.ScrolledText(window,width=30,height=10)
+prog_entry.grid(column=1,row=8)
+
+
+Label(window,text="").grid(column=0,row=9)
+
+out_label = Label(window,text="Upload Output Screenshot : ",anchor=W,justify=LEFT,width=30)
+out_label.grid(column=0,row=10)
+
+out_path_label = Label(window,text="",anchor=W,justify=LEFT,width=30)
+out_path_label.grid(column=1,row=10)
+
+out_btn = Button(window,text="Choose Picture",command=choose_out)
+out_btn.grid(column=2,row=10)
+
+Label(window,text="").grid(column=0,row=11)
+
+concl_label = Label(window,text="Enter Conclusion : ",anchor=W,justify=LEFT,width=30)
+concl_label.grid(column=0,row=12)
+
+concl_entry = Entry(window,width=50)
+concl_entry.grid(column=1,row=12)
+
+Label(window,text="").grid(column=0,row=13)
+
+gen_btn= Button(window,text="Generate File",command=generate_file)
+gen_btn.grid(column=1,row=14)
+window.mainloop()
