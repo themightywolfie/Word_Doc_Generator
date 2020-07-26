c25d6130ae0d25c3a10db1885f4860cf81ba7728
sam2khatri <samakshkhatri14@gmail.com>
2020-07-26 22:45:47 +0530

    Created package for Python program

---
+++ Practical-File-Generator/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='Practical-File-Generator',
+version='0.1',
+description='Testing Package',
+url='https://github.com/sam2khatri/Word_Doc_Generator',
+author='samaksh',
+author_email='samakshkhatri14@gmail.com',
+license='MIT',
+packages=setuptools.find_packages(),
+zip_safe=False)
